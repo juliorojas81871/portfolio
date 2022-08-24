13361305a63c23372a64e20e99b293d2020dbf27
Julio <juliorojas81871@gmail.com>
2022-08-24 16:59:54 -0400

    adding another project

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,6 +1,7 @@
 import ToDo from '../assets/to-do.jpg'
 import Notes from '../assets/notes.jpg'
 import Secrets from '../assets/secrets.jpg'
+import Dashboard from '../assets/dashboard.jpg'
 
 export const ProjectList = [
     {
@@ -23,5 +24,12 @@ export const ProjectList = [
         skills: "EJS,Node.JS,Passport,Google Outh 2.0,MongoDB,HTML,CSS,JavaScript",
         urlCode: "https://github.com/juliorojas81871/secrets",
         urlLive: "https://secrets--app.herokuapp.com/"
+    },
+    {
+        name: "Dashboard",
+        image: Dashboard,
+        skills: "JavaScript,React,Tailwind,HTML,CSS,Syncfusion",
+        urlCode: "https://github.com/juliorojas81871/dashboard ",
+        urlLive: "https://juliorojas81871.github.io/dashboard/"
     }
 ]
