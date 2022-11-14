e2654cc923c67f0535b3a9140a238e8b896d49c2
Julio Rojas <juliorojas81871@gmail.com>
2022-11-14 18:54:01 -0500

    css work

--- components/About.tsx
+++ components/About.tsx
@@ -22,15 +22,15 @@ const About = (props: Props) => {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src="/assets/example2.jpg"
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px] object-cover"
+        className="-mb-32 sm:-mb-20 md:mb-0 flex-shrink-0 w-40 h-40 sm:w-56 sm:h-56 rounded-full md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px] object-cover"
       />
-      <div className="space-y-10 px-0 md:px-10">
-        <h4 className="text-4xl font-semibold">
+      <div className="space-y-6 sm:space-y-10 px-0 md:px-10">
+        <h4 className="text-2xl sm:text-4xl font-semibold">
           Here is a{" "}
           <span className="decoration-[#F7AB0A]/50 underline">little</span>{" "}
           background
         </h4>
-        <p className="text-lg">
+        <p className="sm:text-lg">
           I'm a front-end web developer specializing in building exceptional
           digital experiences. Currently, I'm focused on building responsive
           front-end web applications while learning back-end technologies.
--- components/TimeLineCard.tsx
+++ components/TimeLineCard.tsx
@@ -5,7 +5,7 @@ interface Props {}
 
 const ExperienceEducationCard = (props: Props) => {
   return (
-    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[340px] h-[590px] md:h-[650px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[340px] h-[400px] sm:h-[590px] md:h-[650px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-y-scroll scrollbar-hide">
       <motion.img
         initial={{
           y: -100,
@@ -15,13 +15,13 @@ const ExperienceEducationCard = (props: Props) => {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         src="/assets/BCC-Seal.png"
-        className="hidden md:flex md:w-32 md:h-32 rounded-full md:rounded-full w-[100px] h-[100px] object-cover object-center"
+        className="flex md:w-32 md:h-32 rounded-full md:rounded-full w-[100px] h-[100px] object-cover object-center"
       />
       <div className="px-0 md:px-10 ">
-        <h4 className="text-3xl md:text-4xl font-light">
+        <h4 className="text-2xl sm:text-3xl md:text-4xl font-light">
           Associate in Science, Major in Computer Science
         </h4>
-        <p className="font-bold text-2xl mt-1">Bergen Community College</p>
+        <p className="font-bold sm:text-2xl mt-1">Bergen Community College</p>
         <div className="flex space-x-2 my-2">
           {/* tech used */}
           <Image
@@ -35,12 +35,12 @@ const ExperienceEducationCard = (props: Props) => {
         <p className="uppercase py-5 text-gray-300">
           September 2015 - December 2018
         </p>
-        <ul className="list-disc space-y-4 ml-5 text-lg">
+        <ul className="list-disc space-y-4 ml-5 sm:text-lg">
           <li>Summary points</li>
           <li>Summary points</li>
           <li>Summary points</li>
           <li>Summary points</li>
-          <li>Summary points</li>
+          <li>Summary pointsSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary pointSummary point</li>
         </ul>
       </div>
     </article>
