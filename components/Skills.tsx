8ed4770fea807f5e7c2c65711ef343d11d2d6416
Julio Rojas <juliorojas81871@gmail.com>
2025-03-25 19:47:38 -0400

    some work and hoefully node fix

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -66,7 +66,7 @@ const Skills = ({ skills }: Props) => {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Skills
       </h3>
-      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-sm">
         <span>{displayedText}</span>
         <Cursor cursorColor="#F7AB0A" />
       </h3>
@@ -95,8 +95,7 @@ const Skills = ({ skills }: Props) => {
             onMouseLeave={() => setHoveredSkill(null)}
           >
             <Skill 
-              skill={skill} 
-              directionLeft 
+              skill={skill}  
               isActive={!hoveredSkill && index + skills.length / 2 === currentSkillIndex} 
               isHovered={hoveredSkill === skill.title}
             />
